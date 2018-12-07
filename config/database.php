2bdfe640e79fda26242e428b819eb599767936d1
Breno Mol <emaildobrenomol@gmail.com>
2018-12-07 16:47:23 -0200

    Configuracao da base de dados

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '108.179.253.175'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'amppl450_training_system'),
+            'username' => env('DB_USERNAME', 'amppl450_ampplie'),
+            'password' => env('DB_PASSWORD', 'Xt6^~WF.yF;;'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
